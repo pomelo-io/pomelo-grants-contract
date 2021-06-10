c24c1bf7be385ab16617e5030a306238d0940ae5
YaroShkvorets <shkvorets@gmail.com>
2021-06-10 15:49:08 -0400

    refactor on_social

--- src/notifiers.cpp
+++ src/notifiers.cpp
@@ -35,42 +35,52 @@ void pomelo::on_transfer( const name from, const name to, const asset quantity, 
 [[eosio::on_notify("*::social")]]
 void pomelo::on_social( const name user_id, const set<name> socials )
 {
-    // const auto round_id = get_key_value("round.id"_n);
-    // pomelo::rounds_table _rounds( get_self(), get_self().value );
-    // pomelo::match_table _match( get_self(), round_id );
+    const auto round_id = get_key_value("round.id"_n);
 
-    // if ( round_id == 0 || get_first_receiver() != LOGIN_CONTRACT ) return;
+    if ( round_id == 0 || get_first_receiver() != LOGIN_CONTRACT ) return;
 
-    // const auto round_itr = _rounds.find( round_id );
-    // if (round_itr == _rounds.end() || round_itr->user_ids.count( user_id ) == 0) return;
+    pomelo::rounds_table _rounds( get_self(), get_self().value );
+    pomelo::match_table _match( get_self(), round_id );
+    pomelo::users_table _users( get_self(), round_id );
 
-    // const auto multiplier = get_user_boost_mutliplier( user_id );
+    const auto round_itr = _rounds.find( round_id );
+    if (round_itr == _rounds.end() || get_index( round_itr->user_ids, user_id ) == -1) return;
 
-    // for (const auto grant_id: round_itr->grant_ids) {
-    //     const auto match_itr = _match.find( grant_id.value );
-    //     if (match_itr->user_multiplier.count( user_id ) == 0 || match_itr->user_multiplier.at( user_id ) == multiplier) continue;
+    const auto new_multiplier = get_user_boost_mutliplier( user_id );
 
-    //     const auto old_boost = match_itr->user_boost.at(user_id);
-    //     const auto old_square = match_itr->square;
-    //     const auto boost = match_itr->user_value.at(user_id) * multiplier;
-    //     const auto user_sqrt = sqrt( match_itr->user_value.at(user_id) + boost );
-    //     const auto sum_boost = match_itr->sum_boost - old_boost + boost;
-    //     const auto sum_sqrt = match_itr->sum_sqrt - match_itr->user_sqrt.at(user_id) + user_sqrt;
+    const auto user_itr = _users.find( user_id.value );
+    if (user_itr == _users.end() || user_itr->multiplier == new_multiplier) return;
 
-    //     _match.modify( match_itr, get_self(), [&]( auto & row ) {
-    //         row.user_multiplier[user_id] = multiplier;
-    //         row.user_boost[user_id] = boost;
-    //         row.user_sqrt[user_id] = user_sqrt;
-    //         row.sum_boost = sum_boost;
-    //         row.sum_sqrt = sum_sqrt;
-    //         row.square = sum_sqrt * sum_sqrt;
-    //         row.updated_at = current_time_point();
-    //     });
+    const auto old_multiplier = user_itr->multiplier;
+    for (int i = 0; i < user_itr->contributions.size(); i++) {
 
-    //     _rounds.modify( round_itr, get_self(), [&]( auto & row ) {
-    //         row.sum_boost += boost - old_boost;
-    //         row.sum_square += match_itr->square - old_square;
-    //         row.updated_at = current_time_point();
-    //     });
-    // }
+        const auto old_value = user_itr->contributions[i].value;
+        const auto donated = old_value / (1 + old_multiplier);
+        const auto new_value = donated * (1 + new_multiplier);
+        const auto old_boost = donated * old_multiplier;
+        const auto new_boost = donated * new_multiplier;
+
+        const auto match_itr = _match.find( user_itr->contributions[i].id.value );
+        double old_square = match_itr->square, new_square = 0;
+
+        _match.modify( match_itr, get_self(), [&]( auto & row ) {
+            row.sum_boost += new_boost - old_boost;
+            row.sum_sqrt += sqrt(new_value) - sqrt(old_value);
+            new_square = row.square = row.sum_sqrt * row.sum_sqrt;
+            row.updated_at = current_time_point();
+        });
+
+        _rounds.modify( round_itr, get_self(), [&]( auto & row ) {
+            row.sum_boost += new_boost - old_boost;
+            row.sum_square += new_square - old_square;
+            row.updated_at = current_time_point();
+        });
+
+        _users.modify( user_itr, get_self(), [&]( auto & row ) {
+            row.multiplier = new_multiplier;
+            row.contributions[i].value = new_value;
+            row.updated_at = current_time_point();
+        });
+    }
+
 }
