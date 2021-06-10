caa0abe9ca727109c2af5e242896212e99157af8
YaroShkvorets <shkvorets@gmail.com>
2021-06-10 16:18:25 -0400

    fix tests

--- test.sh
+++ test.sh
@@ -4,5 +4,4 @@ cleos wallet unlock --password $(cat ~/eosio-wallet/.pass)
 bats ./__tests__/system.bats
 bats ./__tests__/config.bats
 bats ./__tests__/users.bats
-# bats ./__tests__/projects.bats
-bats ./__tests__/projects2.bats
+bats ./__tests__/projects.bats
