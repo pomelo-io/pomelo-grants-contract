f9fe7d5933c92889aa5f9cdbfe736284ca1dd4c5
Denis Carriere <carriere.denis@gmail.com>
2021-06-30 13:27:48 -0400

    update test

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,9 @@ cleos wallet unlock --password $(cat ~/eosio-wallet/.pass)
 
 # create matching round and start it
 cleos push action app.pomelo setround '[1, "2021-06-01T00:00:00", "2021-09-01T00:00:00"]' -p app.pomelo
-cleos push action app.pomelo init '[1, 1]' -p app.pomelo
+cleos push action app.pomelo init '[]' -p app.pomelo
+cleos push action app.pomelo setconfig '["status", 1]' -p app.pomelo
+cleos push action app.pomelo setconfig '["roundid", 1]' -p app.pomelo
 
 # create grant, enable it and join round
 cleos push action app.pomelo setgrant '["grant1", "prjman.eosn", ["prjman.eosn"], "prjgrant", [["4,EOS", "eosio.token"]]]' -p app.pomelo
