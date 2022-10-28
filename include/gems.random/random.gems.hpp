c6731063c47dec64ac19531d25f0467519182ad0
YaroShkvorets <shkvorets@gmail.com>
2022-10-27 22:53:24 -0400

    update random

--- include/gems.random/random.gems.hpp
+++ include/gems.random/random.gems.hpp
@@ -14,6 +14,7 @@ namespace random {
         size_t size = transaction_size();
         char buf[size];
         size_t read = read_transaction( buf, size );
+        check( size == read, "random::get_trx_id: read_transaction failed");
         return sha256( buf, read );
     }
 
